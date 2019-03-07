6ac56e906df589e85ddfdff5adc8296101279af3
Coloradosunrae <stevie@ourhopecenter.org>
2019-03-06 22:36:16 -0700

    inital

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hot & Spicy Burger', false);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Buffalo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Random Burger', false);
